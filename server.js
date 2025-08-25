b4860b1e2cba155a822a32d852d9b8a044c9a822
suraj <suraj.tech.in@gmail.com>
2025-08-25 23:34:00 +0545

    setup for production

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.delete('/api/notes/:id',(req,res)=>{
 
 app.use(unknownEndpoint)
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT,()=>{
   console.log(`Server running in port ${PORT}`);
